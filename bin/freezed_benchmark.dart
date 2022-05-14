8686189d5d29b4acf92bdcb4c6464ac02fd6ece8
fzyzcjy <ch271828n@outlook.com>
2022-05-14 19:17:56 +0800

    add dependency

--- bin/freezed_benchmark.dart
+++ bin/freezed_benchmark.dart
@@ -1,5 +1,3 @@
-import 'package:freezed_benchmark/freezed_benchmark.dart' as freezed_benchmark;
-
 void main(List<String> arguments) {
-  print('Hello world: ${freezed_benchmark.calculate()}!');
+  TODO;
 }
--- test/freezed_benchmark_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_benchmark/freezed_benchmark.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
